70be5bbb44868fd708925eea3efd068b0a77f1eb
Marko Djuranovic <146464812+MarkoDjuranovic@users.noreply.github.com>
2024-04-25 15:09:00 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Example-PWApnpm%20/sw.js', { scope: '/Example-PWApnpm%20/' })})}
